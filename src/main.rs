bca4fabe7a1e4f69785254060cdaa1334aa38e15
kazanefu <kazanefu12@gmail.com>
2026-05-31 18:13:10 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use bevy::{prelude::*, window::WindowResolution};
+use bevy_hanabi::prelude::*;
+use bevy_rapier2d::prelude::*;
+mod settings;
+fn main() {
+    let settings = settings::get_settings();
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: settings.window.title.clone(),
+            resolution: WindowResolution::new(settings.window.width, settings.window.height),
+            mode: if settings.window.fullscreen {
+                bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Primary)
+            } else {
+                bevy::window::WindowMode::Windowed
+            },
+            present_mode: if settings.window.vsync {
+                bevy::window::PresentMode::AutoVsync
+            } else {
+                bevy::window::PresentMode::AutoNoVsync
+            },
+            ..default()
+        }),
+        ..default()
+    }));
+    app.add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .insert_resource(TimestepMode::Interpolated {
+            dt: 1.0 / 120.0,
+            time_scale: 1.0,
+            substeps: 1,
+        })
+        // .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(HanabiPlugin)
+        .insert_resource(settings)
+        .run();
+}
---
+++ src/settings/mod.rs
@@ -0,0 +1,53 @@
+use bevy::prelude::Resource;
+use serde::{Deserialize, Serialize};
+mod load;
+
+pub use load::get_settings;
+
+#[derive(Resource, Default, Debug, Clone, Deserialize, Serialize)]
+pub struct Settings {
+    pub window: WindowSettings,
+    pub graphics: GraphicsSettings,
+    pub audio: AudioSettings,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct WindowSettings {
+    pub width: u32,
+    pub height: u32,
+    pub title: String,
+    pub fullscreen: bool,
+    pub vsync: bool,
+}
+
+impl Default for WindowSettings {
+    fn default() -> Self {
+        Self {
+            width: 800,
+            height: 600,
+            title: "phantom_ta".into(),
+            fullscreen: false,
+            vsync: true,
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone, Deserialize, Serialize)]
+pub struct GraphicsSettings {
+    pub light_background: bool,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct AudioSettings {
+    pub bgm_volume: f32,
+    pub se_volume: f32,
+}
+
+impl Default for AudioSettings {
+    fn default() -> Self {
+        Self {
+            bgm_volume: 0.5,
+            se_volume: 0.7,
+        }
+    }
+}

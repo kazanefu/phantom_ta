6122188a1feae786ce40c8f4ab3f4616569c276b
kazanefu <kazanefu12@gmail.com>
2026-06-26 03:26:36 +0900

    add save data

--- src/player/save_data.rs
+++ src/player/save_data.rs
@@ -1,9 +1,46 @@
 use super::*;
 
-#[derive(Resource, Default)]
+#[derive(Resource)]
+pub struct PlayerSaveDataList {
+    pub list: Vec<PlayerSaveData>,
+}
+
+impl PlayerSaveDataList {
+    pub fn set_id(&mut self) {
+        for (i, data) in self.list.iter_mut().enumerate() {
+            data.id = i;
+        }
+    }
+}
+
+#[derive(Resource)]
 pub struct PlayerSaveData {
+    pub id: usize,
+    pub name: String,
     level: f32,
     available_skills: [bool; skills::SkillKind::NUM_SKILLS],
     skill_build: skills::SkillBuild,
     available_checkpoints: HashSet<usize>,
+    last_checkpoint: usize,
+}
+
+impl Default for PlayerSaveData {
+    fn default() -> Self {
+        const AVAILABLE_SKILLS: [bool; skills::SkillKind::NUM_SKILLS] = {
+            let mut temp = [false; skills::SkillKind::NUM_SKILLS];
+            temp[0] = true;
+            temp
+        };
+        let mut available_checkpoints = HashSet::new();
+        available_checkpoints.insert(0);
+        Self {
+            id: 0,
+            name: "new data".to_string(),
+            level: 1.0,
+            available_skills: AVAILABLE_SKILLS,
+            skill_build: skills::SkillBuild::default(),
+            available_checkpoints: available_checkpoints,
+            last_checkpoint: 0,
+        }
+    }
 }
